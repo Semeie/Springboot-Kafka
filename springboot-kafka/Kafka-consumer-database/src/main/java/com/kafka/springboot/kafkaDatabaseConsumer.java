b1cee060be51a94427a62101b4ba5609f6ee18c1
Seme'e <sam2020tek@gmail.com>
2023-03-29 23:09:30 -0400

    added and configured MySQL database

--- springboot-kafka/Kafka-consumer-database/src/main/java/com/kafka/springboot/kafkaDatabaseConsumer.java
+++ springboot-kafka/Kafka-consumer-database/src/main/java/com/kafka/springboot/kafkaDatabaseConsumer.java
@@ -1,5 +1,7 @@
 package com.kafka.springboot;
 
+import com.kafka.springboot.entity.WikimediaData;
+import com.kafka.springboot.repository.WikimediaDataRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -9,12 +11,22 @@ import org.springframework.stereotype.Service;
 public class kafkaDatabaseConsumer {
     private static final Logger LOGGER = LoggerFactory.getLogger(kafkaDatabaseConsumer.class);
 
+    private WikimediaDataRepository dataRepository;
+
+    public kafkaDatabaseConsumer(WikimediaDataRepository dataRepository) {
+        this.dataRepository = dataRepository;
+    }
+
     @KafkaListener(
         topics = "wikimedia_recentchange",
         groupId = "myGroup"
     )
     public void consumer(String eventMessage){
         LOGGER.info(String.format("Event message received -> %s", eventMessage));
+
+        WikimediaData wikimediaData = new WikimediaData();
+        wikimediaData.setWikiEventData(eventMessage);
+        dataRepository.save(wikimediaData);
     }
 
 }
